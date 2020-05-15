9c5daa80522f91d5846b17d06cf27c6b7c4d750f
Neimerc <Neil_Mercury@yahoo.es>
2020-05-15 11:11:18 +0200

    added custom name for discriminatorColumn

--- src/main/java/com/neimerc/hibernate/demo/entity/User.java
+++ src/main/java/com/neimerc/hibernate/demo/entity/User.java
@@ -1,6 +1,8 @@
 package com.neimerc.hibernate.demo.entity;
 
 import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,6 +14,8 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "user")
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "USER_TYPE", 
+						discriminatorType = DiscriminatorType.STRING)
 public abstract class User {
 
 	@Id
